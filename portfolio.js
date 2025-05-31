9944a6cb7292331a0cba6c236031c44d4d9ddc17
Shifaasherin <shifaasherin4050@gmail.com>
2025-05-31 15:46:46 +0530

    portfolio

---
+++ portfolio.js
@@ -0,0 +1,22 @@
+function sendMail() {
+  var paras = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_gszl08g";
+  const templateID = "template_y93bp4w";
+
+    emailjs.send(serviceID, templateID, paras)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
